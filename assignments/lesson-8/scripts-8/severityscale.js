4df1b91f0c171a4c50493d16208f051adcfeeaee
gingersmash <35277674+gingersmash@users.noreply.github.com>
2018-03-03 20:53:25 -0600

    Lesson 8 materials first push

---
+++ assignments/lesson-8/scripts-8/severityscale.js
@@ -0,0 +1,8 @@
+var slider = document.getElementById("rating");
+var output = document.getElementById("ratingvalue");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
